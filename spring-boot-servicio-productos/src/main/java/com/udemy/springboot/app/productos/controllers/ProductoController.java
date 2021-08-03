358e2b139faf783908e330c09c8da467fb5b274c
Carlos Zapata <carlos.gomez@codesa.com.co>
2021-08-03 15:55:00 -0500

    Agregada configuracion Spring Cloud Gateway

--- spring-boot-servicio-productos/src/main/java/com/udemy/springboot/app/productos/controllers/ProductoController.java
+++ spring-boot-servicio-productos/src/main/java/com/udemy/springboot/app/productos/controllers/ProductoController.java
@@ -6,8 +6,12 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.springboot.app.productos.models.entity.Producto;
@@ -65,6 +69,12 @@ public class ProductoController {
 			e.printStackTrace();
 		}*/
 		return producto;
+	}
+	
+	@PostMapping("/crear")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto crear(@RequestBody Producto producto) {
+		return productoService.save(producto);		
 	}
 	
 	 
